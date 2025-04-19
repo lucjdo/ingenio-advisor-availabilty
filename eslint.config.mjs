f3983aaf2d7d0d6f34894fc95258564f6600241e
lucjdo <luciano.sejudo@gmail.com>
2025-04-19 16:48:39 -0300

    Format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "prettier"
-  ),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
     plugins: {
-      prettier: require("eslint-plugin-prettier"),
+      prettier: require('eslint-plugin-prettier')
     },
     rules: {
-      "prettier/prettier": "error",
-      "arrow-body-style": ["error", "as-needed"],
-      "prefer-arrow-callback": ["error", { "allowNamedFunctions": false }],
-    },
-  },
-];
+      'prettier/prettier': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-arrow-callback': ['error', { allowNamedFunctions: false }]
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
