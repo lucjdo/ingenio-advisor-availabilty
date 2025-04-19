b452a83ec3469d86a08a39b566ff84e1c2b16fe9
lucjdo <33455403+lucjdo@users.noreply.github.com>
2025-04-19 16:56:36 -0300

    chore: set up project (#1)
    * Install Material UI
    
    * Create Github actions
    
    * Clean up repository
    
    * Install prettier
    
    * Format code
    
    * Change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-arrow-callback': ['error', { allowNamedFunctions: false }]
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
