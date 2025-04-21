6c3238a0095427bf384953ee94db5a856e41af66
lucjdo <luciano.sejudo@gmail.com>
2025-04-21 01:13:03 -0300

    add vitest and advisors page simple unit test

--- app/advisors/page.tsx
+++ app/advisors/page.tsx
@@ -7,9 +7,7 @@ export default async function AdvisorsPage() {
 
   return (
     <Container className='py-9' maxWidth='lg'>
-      <Typography variant='h3' component='h1' gutterBottom>
-        Our Advisors
-      </Typography>
+      <h1>Our Advisors</h1>
       <Typography sx={{ mb: 3 }} variant='subtitle1' color='text.secondary'>
         Browse our expert advisors and find the perfect match for your needs
       </Typography>
