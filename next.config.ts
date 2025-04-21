1d0459eabab260828c1f93947c21cc0bc1b0a91d
lucjdo <luciano.sejudo@gmail.com>
2025-04-20 21:00:58 -0300

    add known image domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['si.keen.com']
+  }
 }
 
 export default nextConfig
