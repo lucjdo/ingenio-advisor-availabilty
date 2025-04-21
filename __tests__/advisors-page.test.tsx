49c86e3cfd4f3489c25bb09b1fa7f7ae1072c7c0
lucjdo <33455403+lucjdo@users.noreply.github.com>
2025-04-21 01:20:21 -0300

    feature: add unit testing starting config (#3)
    * add vitest and advisors page simple unit test
    
    * add test gh action

---
+++ __tests__/advisors-page.test.tsx
@@ -0,0 +1,26 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AdvisorsPage from '@/app/advisors/page'
+
+test('Advisors Page', async () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      }
+    }
+  })
+
+  const advisorsPageContent = await AdvisorsPage()
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      {advisorsPageContent}
+    </QueryClientProvider>
+  )
+
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Our Advisors' })
+  ).toBeDefined()
+})
---
+++ vitest.config.mts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/**',
+        '.next/**',
+        'coverage/**',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/eslint.config.mjs',
+        'public/**'
+      ]
+    }
+  }
+})
